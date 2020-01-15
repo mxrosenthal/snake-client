aa02391abe4250b05d1964c2aeb1b763c814018c
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-15 21:46:29 +0000

    made inputs.js to handle all inputs which get exported to play.js. play.js got refactored and minimized.

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupIput = function() {
+  console.log('stting up!');
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    console.log('Goodbye');
+    process.exit();
+  }
+}
+
+module.exports = {
+  setupIput
+};
--- play.js
+++ play.js
@@ -1,35 +1,7 @@
-// const net = require('net');
-
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-
-const setupIput = function() {
-  console.log('stting up!');
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput)
-
-  return stdin;
-}
-const handleUserInput = function(data) {
-  console.log('data: ', data);
-
-  if (data === '\u0003') {
-    // conn.write('Goodbye');
-    console.log('Goodbye');
-    process.exit();
-  }
-  // return data;
-}
-
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-// connect();
-setupIput();
-let conn = connect();
+connect();
+
+setupInput();
