67d26be5512d787b6e717dee507d8da837f5bf2b
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-15 20:33:12 +0000

    refactored to share functions between play.js and client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.13.4',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
